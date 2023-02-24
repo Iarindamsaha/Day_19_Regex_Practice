be1c7e560a2198a870143aa1062e6ea7b19641b6
Arindam Saha <asaha15071998@gmail.com>
2023-02-24 10:19:25 +0530

    Checking Phone Number

--- src/com/regex/UserRegistrationRegex.java
+++ src/com/regex/UserRegistrationRegex.java
@@ -41,12 +41,28 @@ public class UserRegistrationRegex {
         System.out.print("Enter Your Email Address : ");
         String email = sc.nextLine();
 
-        if (Pattern.matches("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+(@)[a-z0-9]+[.][a-z]+[.]?[a-z]+$", email)) {
+        if (Pattern.matches("^[a-zA-Z]+[a-zA-Z0-9]*[-.+_]?[a-zA-Z0-9]+(@)[a-z0-9]+[.][a-z]+[.]?[a-z]+$", email)) {
 
             System.out.println("Check : Email is Valid");
         } else {
             System.out.println("Check : Email Invalid");
         }
+
+        //UC4
+        System.out.println();
+
+        System.out.print("Enter Your PhoneNumber : ");
+        String phoneNumber = sc.nextLine();
+
+        if (Pattern.matches("^[1-9]{2}\\s[0-9]{10}$", phoneNumber)) {
+
+            System.out.println("Check : Phone Number is Valid ");
+        } else {
+            System.out.println("Check : Phone Number is Invalid ");
+        }
+
+
+
 
 
 
