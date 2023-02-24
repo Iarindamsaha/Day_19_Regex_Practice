ad46e5edea79604cc11a18cd1971bda0f17562fe
Arindam Saha <asaha15071998@gmail.com>
2023-02-24 09:26:42 +0530

    Welcome Main

---
+++ src/com/regex/UserRegistrationRegex.java
@@ -0,0 +1,11 @@
+package com.regex;
+
+public class UserRegistrationRegex {
+
+    public static void main(String[] args) {
+
+        System.out.println("---Welcome To The User Registration Program---");
+
+    }
+
+}
